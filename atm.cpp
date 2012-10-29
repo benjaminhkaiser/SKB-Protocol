9b8760850724b0883231ea23c11b9c181a62c853
Benjamin Kaiser <benjamin.h.kaiser@gmail.com>
2012-10-29 14:17:04 -0400

    Can send commands from atm -> bank (see desc)
    Set up connection between atm and bank
    Can successfully parse login (and user/pin) and verify that bank
    recieves
    Can't set up any actions on the bank side until the Account Class is
    implemented
    PIN inputs as ***
    Nothing is encrypted yet - it's all sending as plaintext
    -Sam & Ben

--- atm.cpp
+++ atm.cpp
@@ -1,6 +1,6 @@
 /**
-	@file atm.cpp
-	@brief Top level ATM implementation file
+    @file atm.cpp
+    @brief Top level ATM implementation file
  */
 #include <unistd.h>
 #include <sys/socket.h>
@@ -10,85 +10,181 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <iostream>
+#include <string>
+#include <termios.h>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+//This reads in each character to be masked
+int getch() {
+    int ch;
+    struct termios t_old, t_new;
+
+    tcgetattr(STDIN_FILENO, &t_old);
+    t_new = t_old;
+    t_new.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &t_new);
+
+    ch = getchar();
+
+    tcsetattr(STDIN_FILENO, TCSANOW, &t_old);
+    return ch;
+}
+
+/*
+    This function prints the string "PIN: " and gets the user entered
+    pin while replacing each character with '*'
+*/
+std::string getpass(const char *prompt, bool show_asterisk=true)
+{
+  const char BACKSPACE=127;
+  const char RETURN=10;
+
+  std::string password;
+  unsigned char ch=0;
+
+  cout << prompt;
+  //Consume previous newline
+  ch = getch();
+  while((ch=getch())!=RETURN)
+    {
+       if(ch==BACKSPACE)
+         {
+            if(password.length()!=0)
+              {
+                 if(show_asterisk)
+                 cout <<"\b \b";
+                 password.resize(password.length()-1);
+              }
+         }
+       else
+         {
+             password+=ch;
+             if(show_asterisk)
+                 cout <<'*';
+         }
+    }
+    
+  return password;
+}
+
 
 int main(int argc, char* argv[])
 {
-	if(argc != 2)
-	{
-		printf("Usage: atm proxy-port\n");
-		return -1;
-	}
-	
-	//socket setup
-	unsigned short proxport = atoi(argv[1]);
-	int sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	if(!sock)
-	{
-		printf("fail to create socket\n");
-		return -1;
-	}
-	sockaddr_in addr;
-	addr.sin_family = AF_INET;
-	addr.sin_port = htons(proxport);
-	unsigned char* ipaddr = reinterpret_cast<unsigned char*>(&addr.sin_addr);
-	ipaddr[0] = 127;
-	ipaddr[1] = 0;
-	ipaddr[2] = 0;
-	ipaddr[3] = 1;
-	if(0 != connect(sock, reinterpret_cast<sockaddr*>(&addr), sizeof(addr)))
-	{
-		printf("fail to connect to proxy\n");
-		return -1;
-	}
-	
-	//input loop
-	char buf[80];
-	while(1)
-	{
-		printf("atm> ");
-		fgets(buf, 79, stdin);
-		buf[strlen(buf)-1] = '\0';	//trim off trailing newline
-		
-		//TODO: your input parsing code has to put data here
-		char packet[1024];
-		int length = 1;
-		
-		//input parsing
-		if(!strcmp(buf, "logout"))
-			break;
-		//TODO: other commands
-		
-		//send the packet through the proxy to the bank
-		if(sizeof(int) != send(sock, &length, sizeof(int), 0))
-		{
-			printf("fail to send packet length\n");
-			break;
-		}
-		if(length != send(sock, (void*)packet, length, 0))
-		{
-			printf("fail to send packet\n");
-			break;
-		}
-		
-		//TODO: do something with response packet
-		if(sizeof(int) != recv(sock, &length, sizeof(int), 0))
-		{
-			printf("fail to read packet length\n");
-			break;
-		}
-		if(length >= 1024)
-		{
-			printf("packet too long\n");
-			break;
-		}
-		if(length != recv(sock, packet, length, 0))
-		{
-			printf("fail to read packet\n");
-			break;
-		}
-	}
-	
-	//cleanup
-	close(sock);
-	return 0;
+    if(argc != 2)
+    {
+        printf("Usage: atm proxy-port\n");
+        return -1;
+    }
+    
+    //socket setup
+    unsigned short proxport = atoi(argv[1]);
+    int sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if(!sock)
+    {
+        printf("fail to create socket\n");
+        return -1;
+    }
+    sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(proxport);
+    unsigned char* ipaddr = reinterpret_cast<unsigned char*>(&addr.sin_addr);
+    ipaddr[0] = 127;
+    ipaddr[1] = 0;
+    ipaddr[2] = 0;
+    ipaddr[3] = 1;
+    if(0 != connect(sock, reinterpret_cast<sockaddr*>(&addr), sizeof(addr)))
+    {
+        printf("fail to connect to proxy\n");
+        return -1;
+    }
+    
+    //input loop
+    //****************Should have boolean for successful login********************!
+    char buf[80];
+    while(1)
+    {
+        printf("atm> ");
+        fgets(buf, 79, stdin);
+        buf[strlen(buf)-1] = '\0';  //trim off trailing newline
+        
+        //TODO: your input parsing code has to put data here
+        //Make sure to check buffer overflow
+        
+        //login [username]
+        
+        char packet[1024];
+        int length = 1;
+        
+        //input parsing
+        if(!strcmp(buf, "logout"))
+            break;
+        else if(!strcmp(buf, "login")){    //test login functionality
+            //this block prompts for 30 char username for login and puts it in the username var
+            std::string username;
+            cout << "user: ";
+            cin >> username;
+            username = username.substr(0,30);
+            
+            //this block prompts for PIN for login and puts it in the pin var
+            std::string pin;
+            pin = getpass("PIN: ", true);
+            pin = pin.substr(0,4);
+          
+            strcpy(packet,buf);
+            for(unsigned int i = 0; i < username.length(); ++i)
+            {
+                packet[strlen(buf) + i] = username[i];
+            } //end for appened info to packet
+            for(unsigned int i = 0; i < 4; ++i)
+            {
+                packet[strlen(buf) + username.length() + i] = pin[i];
+            } //end for append pin to packet
+            //Add the terminating newline
+            packet[strlen(buf) + username.length() + 4] = '\0';
+            //printf(packet);
+           
+        } //end else if login   
+        
+        //TODO: other commands
+        
+        //send the packet through the proxy to the bank
+        //There are two send messages - 1) packet length and 2) actual packet
+        
+        length = strlen(packet);
+        if(sizeof(int) != send(sock, &length, sizeof(int), 0))
+        {
+            printf("fail to send packet length\n");
+            break;
+        }
+        if(length != send(sock, (void*)packet, length, 0))
+        {
+            printf("fail to send packet\n");
+            break;
+        }
+        
+        //TODO: do something with response packet
+        if(sizeof(int) != recv(sock, &length, sizeof(int), 0))
+        {
+            printf("fail to read packet length\n");
+            break;
+        }
+        if(length >= 1024)
+        {
+            printf("packet too long\n");
+            break;
+        }
+        if(length != recv(sock, packet, length, 0))
+        {
+            printf("fail to read packet\n");
+            break;
+        }
+    }
+    
+    //cleanup
+    close(sock);
+    return 0;
 }
--- bank.cpp
+++ bank.cpp
@@ -1,6 +1,6 @@
 /**
-	@file bank.cpp
-	@brief Top level bank implementation file
+    @file bank.cpp
+    @brief Top level bank implementation file
  */
 #include <unistd.h>
 #include <sys/socket.h>
@@ -17,117 +17,119 @@ void* console_thread(void* arg);
 
 int main(int argc, char* argv[])
 {
-	if(argc != 2)
-	{
-		printf("Usage: bank listen-port\n");
-		return -1;
-	}
-	
-	unsigned short ourport = atoi(argv[1]);
-	
-	//socket setup
-	int lsock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	if(!lsock)
-	{
-		printf("fail to create socket\n");
-		return -1;
-	}
-	
-	//listening address
-	sockaddr_in addr_l;
-	addr_l.sin_family = AF_INET;
-	addr_l.sin_port = htons(ourport);
-	unsigned char* ipaddr = reinterpret_cast<unsigned char*>(&addr_l.sin_addr);
-	ipaddr[0] = 127;
-	ipaddr[1] = 0;
-	ipaddr[2] = 0;
-	ipaddr[3] = 1;
-	if(0 != bind(lsock, reinterpret_cast<sockaddr*>(&addr_l), sizeof(addr_l)))
-	{
-		printf("failed to bind socket\n");
-		return -1;
-	}
-	if(0 != listen(lsock, SOMAXCONN))
-	{
-		printf("failed to listen on socket\n");
-		return -1;
-	}
-	
-	pthread_t cthread;
-	pthread_create(&cthread, NULL, console_thread, NULL);
-	
-	//loop forever accepting new connections
-	while(1)
-	{
-		sockaddr_in unused;
-		socklen_t size = sizeof(unused);
-		int csock = accept(lsock, reinterpret_cast<sockaddr*>(&unused), &size);
-		if(csock < 0)	//bad client, skip it
-			continue;
-			
-		pthread_t thread;
-		pthread_create(&thread, NULL, client_thread, (void*)csock);
-	}
+    if(argc != 2)
+    {
+        printf("Usage: bank listen-port\n");
+        return -1;
+    }
+    
+    unsigned short ourport = atoi(argv[1]);
+    
+    //socket setup
+    int lsock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if(!lsock)
+    {
+        printf("fail to create socket\n");
+        return -1;
+    }
+    
+    //listening address
+    sockaddr_in addr_l;
+    addr_l.sin_family = AF_INET;
+    addr_l.sin_port = htons(ourport);
+    unsigned char* ipaddr = reinterpret_cast<unsigned char*>(&addr_l.sin_addr);
+    ipaddr[0] = 127;
+    ipaddr[1] = 0;
+    ipaddr[2] = 0;
+    ipaddr[3] = 1;
+    if(0 != bind(lsock, reinterpret_cast<sockaddr*>(&addr_l), sizeof(addr_l)))
+    {
+        printf("failed to bind socket\n");
+        return -1;
+    }
+    if(0 != listen(lsock, SOMAXCONN))
+    {
+        printf("failed to listen on socket\n");
+        return -1;
+    }
+    
+    pthread_t cthread;
+    pthread_create(&cthread, NULL, console_thread, NULL);
+    
+    //loop forever accepting new connections
+    while(1)
+    {
+        sockaddr_in unused;
+        socklen_t size = sizeof(unused);
+        int csock = accept(lsock, reinterpret_cast<sockaddr*>(&unused), &size);
+        if(csock < 0)   //bad client, skip it
+            continue;
+            
+        pthread_t thread;
+        pthread_create(&thread, NULL, client_thread, (void*)csock);
+    }
 }
 
 void* client_thread(void* arg)
 {
-	int csock = (int)arg;
-	
-	printf("[bank] client ID #%d connected\n", csock);
-	
-	//input loop
-	int length;
-	char packet[1024];
-	while(1)
-	{
-		//read the packet from the ATM
-		if(sizeof(int) != recv(csock, &length, sizeof(int), 0))
-			break;
-		if(length >= 1024)
-		{
-			printf("packet too long\n");
-			break;
-		}
-		if(length != recv(csock, packet, length, 0))
-		{
-			printf("[bank] fail to read packet\n");
-			break;
-		}
-		
-		//TODO: process packet data
-		
-		//TODO: put new data in packet
-		
-		//send the new packet back to the client
-		if(sizeof(int) != send(csock, &length, sizeof(int), 0))
-		{
-			printf("[bank] fail to send packet length\n");
-			break;
-		}
-		if(length != send(csock, (void*)packet, length, 0))
-		{
-			printf("[bank] fail to send packet\n");
-			break;
-		}
+    int csock = (int)arg;
+    
+    printf("[bank] client ID #%d connected\n", csock);
+    
+    //input loop
+    int length;
+    char packet[1024];
+    while(1)
+    {
+        //read the packet from the ATM
+        if(sizeof(int) != recv(csock, &length, sizeof(int), 0)){
+            printf("you're here");
+            break;
+        }
+        if(length >= 1024)
+        {
+            printf("packet too long\n");
+            break;
+        }
+        if(length != recv(csock, packet, length, 0))
+        {
+            printf("[bank] fail to read packet\n");
+            break;
+        }
+        printf(packet);
+        //TODO: process packet data
+        
+        //TODO: put new data in packet
+        
+        //send the new packet back to the client
+        if(sizeof(int) != send(csock, &length, sizeof(int), 0))
+        {
+            printf("[bank] fail to send packet length\n");
+            break;
+        }
+        if(length != send(csock, (void*)packet, length, 0))
+        {
+            printf("[bank] fail to send packet\n");
+            break;
+        }
 
-	}
+    }
 
-	printf("[bank] client ID #%d disconnected\n", csock);
+    printf("[bank] client ID #%d disconnected\n", csock);
 
-	close(csock);
-	return NULL;
+    close(csock);
+    return NULL;
 }
 
 void* console_thread(void* arg)
 {
-	char buf[80];
-	while(1)
-	{
-		printf("bank> ");
-		fgets(buf, 79, stdin);
-		buf[strlen(buf)-1] = '\0';	//trim off trailing newline
-		
-		//TODO: your input parsing code has to go here
-	}
+    char buf[80];
+    while(1)
+    {
+        printf("bank> ");
+        fgets(buf, 79, stdin);
+        buf[strlen(buf)-1] = '\0';  //trim off trailing newline
+        
+        //TODO: your input parsing code has to go here
+    }
 }
